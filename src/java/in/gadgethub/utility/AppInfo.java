6f8ace4691e425d3a41c048f9438d0b86ccc5687
prince rajput <prince rajput@LAPTOP-N2GI2VLF>
2025-05-25 22:01:39 +0530

    GadgetHub

---
+++ src/java/in/gadgethub/utility/AppInfo.java
@@ -0,0 +1,7 @@
+package in.gadgethub.utility;
+public class AppInfo {
+    public static final String appName="GadgetHub";
+    public static final String appEmail="rajnishkushwaha3757@gmail.com";
+    public static final String appPassword="tobc tzqb wrgt ypmi";
+    
+}
